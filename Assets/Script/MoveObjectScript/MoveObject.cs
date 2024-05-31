f47583486486d5141769af619b418994c50bec2c
천민성 <albert6306@naver.com>
2024-05-31 22:06:55 +0900

    edit player Animation , Attack motion

--- Assets/Script/MoveObjectScript/MoveObject.cs
+++ Assets/Script/MoveObjectScript/MoveObject.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
@@ -6,35 +7,52 @@ using UnityEngine;
 using UnityEngine.UI;
 public class MoveObject : MonoBehaviour
 {
+    bool WaitAnimationFinish;
+
+    [Header("Component")]
+    public Rigidbody2D rg;
+    public Animator ani;
+    public Collider2D attackingPlayer;
+    public GameObject attackShow;
+    public SpriteRenderer playerHand; // 이동방향마다 같이 flipX 해줘야됌
+    public SpriteRenderer sp;
+    public Text text;
+    [Space(9)]
     [Header("Charector Status")]
+    public State state;
     public float speed;
     public float pushPower;
     public float moveX;
     public float moveY;
     public bool isAttackSussecs;
-    public Collider2D attackingPlayer;
     [Space(9)]
 
     [Header("Character Name")]
     [SerializeField] private string userName;
-    public Text text;
+    
 
-    [Space(9)]
 
-    [Header("Attack Showing GameObject")] public GameObject attackShow;
-       void Start(){
+    void Start(){
+        state = State.IDLE;
+        sp = GetComponent<SpriteRenderer>();
+        ani = GetComponent<Animator>();
         text.text = this.gameObject.name;
+        rg = GetComponent<Rigidbody2D>();
     }
     public void Move(){
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
-        
+        SetAnimation(State.MOVE);
         transform.position += new Vector3(moveX , moveY).normalized * speed * Time.deltaTime;
     }
     public IEnumerator Attack(){
         attackShow.SetActive(true);
-        yield return new WaitForSeconds(0.5f);
+        SetAnimation(State.ATTACK);
+        
+        yield return new WaitForSeconds(.5f);
+
         attackShow.SetActive(false);
+        SetAnimation(State.IDLE);
     } 
 
     public Vector2 GetPosition(){
@@ -44,7 +62,40 @@ public class MoveObject : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player"){
             isAttackSussecs = true;
+            Rigidbody2D otherRigidBody = other.GetComponent<MoveObject>().rg;
+            otherRigidBody.AddForce(new Vector2(moveX , moveY) * pushPower , ForceMode2D.Impulse);
+            StartCoroutine(StopObject(otherRigidBody));
             attackingPlayer = other;
         }
     }
+    IEnumerator StopObject(Rigidbody2D rg){
+        yield return new WaitForSeconds(0.3f);
+        rg.velocity = Vector2.zero;
+    }
+    ///<summary>
+    /// 애니메이션을 STATE로 SET해주는 함수
+    ///</summary>
+    public void SetAnimation(State state){
+        this.state = state;
+        
+        if(state == State.ATTACK){
+            ani.SetTrigger("IsAttack");
+        }
+        if(state == State.MOVE || state == State.IDLE){
+            ani.SetInteger("IsRun" , Math.Abs((int)moveX) + Math.Abs((int)moveY));
+        }
+
+        if(moveX < 0){
+            sp.flipX = true;
+            playerHand.flipX = true;
+        }else if(moveX > 0){
+            sp.flipX = false;
+            playerHand.flipX = false;
+        }
+    }
+}
+public enum State{
+    IDLE,
+    ATTACK,
+    MOVE
 }
--- Assets/Script/ServerScript/Socket.cs
+++ Assets/Script/ServerScript/Socket.cs
@@ -1,4 +1,4 @@
-using System;
+ using System;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -9,6 +9,7 @@ using UnityEngine;
 using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
+using UnityEngine.XR;
 using WebSocketSharp;
 /*
 [Serializable]
@@ -61,7 +62,7 @@ public class Socket : MonoBehaviour
         // OnMessage : 서버에서 메세지가 넘어오면 실행되는 함수
         try {
             
-            using(ws = new WebSocket("ws://localhost:8000")) {
+            using(ws = new WebSocket("ws://172.30.1.10:8000")) {
                 
                 ws.OnMessage += (sender , e) => {
                     
@@ -75,9 +76,9 @@ public class Socket : MonoBehaviour
                     }if(data.title.Equals("checkCreateUser")){
                         queue.Enqueue(() => CreateOtherUser(data.id , data.users));
                     }if(data.title.Equals("checkMove")){
-                        Debug.Log($"ID : {data.id} , [{data.x},{data.y}]");
-                        Debug.Log($"otherLength : {other.Length}");
                         queue.Enqueue(() => MoveOtherCharector(data));
+                    }if(data.title.Equals("checkAttack")){
+                        queue.Enqueue(() => AttackShow(data.id , new Vector2(data.x , data.y)));
                     }
                 };
             }
@@ -97,20 +98,34 @@ public class Socket : MonoBehaviour
         }
     }
     void LateUpdate(){
-        queue.Enqueue(() => MoveCharector(int.Parse(this_player.name)));
-        this_player_MoveObject.Move();
+        if(this_player_MoveObject.state != State.ATTACK){
+            queue.Enqueue(() => MoveCharector(int.Parse(this_player.name)));
+            this_player_MoveObject.Move();
+        }
         AttackUser();
     }
+    ///<summary>
+    ///Attack 실행시 호출되는 함수
+    ///</summary>
     public void AttackUser(){
         if(Input.GetKeyDown(KeyCode.Space) && !this_player_MoveObject.attackShow.activeSelf){
             StartCoroutine(this_player_MoveObject.Attack());
-        }
-        if(this_player_MoveObject.isAttackSussecs){
-            Data attackData = new Data("AttackOtherPlayer" , int.Parse(this_player_MoveObject.attackingPlayer.name));
+            Data attackData = new Data("AttackOtherPlayer" , int.Parse(this_player.name) , this_player_MoveObject.moveX , this_player_MoveObject.moveY);
             ws.Send(JsonUtility.ToJson(attackData));
-            this_player_MoveObject.isAttackSussecs = false;
         }
     }
+    ///<summary>
+    ///서버에서 보내준 플레이어의 공격을 다른 클라이언트에서 보여줄때 사용
+    ///</summary>
+    public void AttackShow(int id , Vector2 GetAsix){
+        MoveObject attackingUser = GameObject.Find(id.ToString()).GetComponent<MoveObject>();
+        attackingUser.moveX = GetAsix.x;
+        attackingUser.moveY = GetAsix.y;
+        StartCoroutine(attackingUser.Attack());
+    }
+    ///<summary>
+    ///서버와 연결시 처음 실행
+    ///</summary>
     public void CreateUser(){
         GameObject player = Instantiate(user);
         this_player = player;
@@ -123,6 +138,9 @@ public class Socket : MonoBehaviour
         Data data = new Data("EndConnection" , int.Parse(userName));
         ws.Send(JsonUtility.ToJson(data));
     }
+    ///<summary>
+    ///서버와 다른 플레이어 확인 후 생성
+    ///</summary>
     public void CreateOtherUser(int createUserId , int[] users){
         other = GameObject.FindGameObjectsWithTag("Player");
         if(other.Length != users.Length){
@@ -142,19 +160,33 @@ public class Socket : MonoBehaviour
         }
         other = GameObject.FindGameObjectsWithTag("Player");
     }
+    ///<summary>
+    ///한 유저가 게임을 떠날 시 실행
+    ///</summary>
     public void DeleteUser(int deleteIndex){
         Destroy(GameObject.Find(deleteIndex.ToString()).gameObject);
     }
+    ///<summary>
+    ///캐릭터 이동방향을 서버로 보냄 / 매 프레임마다 호출
+    ///</summary>
     public void MoveCharector(int movingPlayer){
         Vector2 move = this_player_MoveObject.GetPosition();
-        Data data = new Data("PlayerMove" ,movingPlayer, move.x , move.y);
+        Data data = new Data("PlayerMove" ,movingPlayer, move.x , move.y , new Vector2(this_player_MoveObject.moveX , this_player_MoveObject.moveY));
         ws.Send(JsonUtility.ToJson(data));
     }
+    ///<summary>
+    ///다른 캐릭터의 움직임을 받아오는 함수 / 매 프레임마다 호출
+    ///</summary>
     public void MoveOtherCharector(Data data){
         for(int i = 0; i < other.Length; i++){
-            Debug.Log($"OtherName : {other[i].name}");
+            
             if(other[i].name == data.id.ToString()){
+                
                 other[i].transform.position = new Vector3(data.x , data.y);
+                MoveObject otherMoveObj = other[i].GetComponent<MoveObject>();
+                otherMoveObj.SetAnimation(State.MOVE);
+                otherMoveObj.moveX = data.moveXY.x;
+                otherMoveObj.moveY = data.moveXY.y;
                 break;
             }
         }
